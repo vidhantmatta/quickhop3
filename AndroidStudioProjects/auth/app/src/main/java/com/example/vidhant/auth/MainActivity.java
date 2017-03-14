fd729296a6f70ad8e97331b17b305c69b164c345
vidhantmatta <mattavidhant@gmail.com>
2017-03-14 23:40:29 +0530

    Login layout design changed.

--- AndroidStudioProjects/auth/app/src/main/java/com/example/vidhant/auth/MainActivity.java
+++ AndroidStudioProjects/auth/app/src/main/java/com/example/vidhant/auth/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.vidhant.auth;
 
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
